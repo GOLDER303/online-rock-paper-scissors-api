dd9b978c49ba8f4bbb1814cd3be5ea58fb998206
GOLDER303 <szymonstepien@interia.pl>
2023-10-08 16:17:00 +0200

    Add redis database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from "@nestjs/common";
 import { RoomModule } from "./room/room.module";
+import { RedisModule } from "@songkeys/nestjs-redis";
 
 @Module({
-  imports: [RoomModule],
+  imports: [
+    RoomModule,
+    RedisModule.forRoot({
+      config: {
+        host: "localhost",
+        port: 6379,
+      },
+    }),
+  ],
   controllers: [],
   providers: [],
 })
